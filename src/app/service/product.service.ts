4f217f80846a478a8748d325a003650b9af1671b
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-05-19 00:52:57 +0700

    instant search in header(bug: take Rxjs not working), fetch data when search

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -5,34 +5,45 @@ import { DetailProduct } from '../model/detail-product.model';
 import { Product } from '../model/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  private apiServerUrl ='http://localhost:3000';
+  private apiServerUrl = 'http://localhost:3000';
   //private apiServerUrl ='http://localhost:8080';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public getProducts():Observable< Product[]>{
+  public getProducts(): Observable<Product[]> {
     //return this.http.get<Product[]>(`${this.apiServerUrl}/product`);
-    return this.http.get< Product[]>(`${this.apiServerUrl}/product/all`);
-
+    return this.http.get<Product[]>(`${this.apiServerUrl}/product/all`);
   }
 
-  public getDetailProductById(productId:number):Observable< DetailProduct>{
-    return this.http.get< DetailProduct>(`${this.apiServerUrl}/product/`+productId);
-
+  public getDetailProductById(productId: number): Observable<DetailProduct> {
+    return this.http.get<DetailProduct>(
+      `${this.apiServerUrl}/product/` + productId
+    );
   }
 
-  public addProduct(product:  Product):Observable< Product>{
-    return this.http.post< Product>(`${this.apiServerUrl}/product/add`,product);
+  public addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${this.apiServerUrl}/product/add`, product);
   }
 
-  public updateProduct(product:  Product):Observable< Product>{
-    return this.http.put< Product>(`${this.apiServerUrl}/product/update`,product);
+  public updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(
+      `${this.apiServerUrl}/product/update`,
+      product
+    );
   }
 
-  public deleteProduct(productId: number):Observable<void>{
-    return this.http.delete<void>(`${this.apiServerUrl}/product/delete//${productId}`);
+  public deleteProduct(productId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/product/delete//${productId}`
+    );
+  }
+  public searchProducts(key: string): Observable<Product[]> {
+    //return this.http.get<Product[]>(`${this.apiServerUrl}/product`);
+    return this.http.get<Product[]>(
+      `${this.apiServerUrl}/product/instantSearch/query?key=${key}`
+    );
   }
 }
