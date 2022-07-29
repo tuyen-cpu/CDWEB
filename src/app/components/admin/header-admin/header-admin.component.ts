f74672c8bc126a63a06c4fbb4c309e4bd751f090
Tran-Ha-git <hochiminhvilanhtuuvidai@gmail.com>
2022-07-29 10:35:39 +0700

    Update header admin

--- src/app/components/admin/header-admin/header-admin.component.ts
+++ src/app/components/admin/header-admin/header-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { StorageService } from 'src/app/service/storage.service';
 
 @Component({
   selector: 'app-header-admin',
@@ -8,11 +11,33 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class HeaderAdminComponent implements OnInit {
   @Output() newItemEvent = new EventEmitter<boolean>();
   isShowSidebar = true;
-  constructor() {}
+  currentUser: any;
+  constructor(
+    private router: Router,
+    private storageService: StorageService,
+    private authService: AuthService,
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.storageService.currentUser.subscribe((data)=>{
+      this.currentUser=data;
+    });
+  }
   addClassToParent() {
     this.isShowSidebar = !this.isShowSidebar;
     this.newItemEvent.emit(this.isShowSidebar);
+  }
+
+  public logout():void{
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
   }
 }
