9050888554391a1a9135c39b79e243875aeda326
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-08-02 02:15:05 +0700

    fixed show in search page

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -123,13 +123,11 @@ export class ProductService {
   }
 
   public updateProduct(product: ProductAdd): Observable<Product> {
-    console.log('sp muon suawL:', product);
     {
       return this.http
         .put<Product>(`${this.apiServerUrl}/product/update`, product)
         .pipe(
           tap((resp) => {
-            console.log(console.log('say khi sua:', resp));
             this.productManager = this.productManager.map((product) =>
               product.id === resp.id ? { ...product, ...resp } : { ...product }
             );
